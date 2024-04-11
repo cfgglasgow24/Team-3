e70ae1af4615a493e8a72393523f0cc5b4b2ba81
Kyle <kylesbowden@gmail.com>
2024-04-11 17:03:37 +0100

    Resolved conflict requests

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/templates/learn.html")
+def learn():
     letter = Question.get_random_letter()
     question = Question(letter)
     return render_template("learn.html", question=question)
